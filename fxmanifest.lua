d931ac50774c0c565006a23067fb05637849d36c
Artmines <arturowells@gmail.com>
2026-04-27 23:43:03 -0230

    feat(init):Initial version

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version("cerulean")
+games({ "gta5" })
+lua54("yes")
+
+client_script("@pulsar-core/exports/cl_error.lua")
+client_script("@pulsar-pwnzor/client/check.lua")
+
+client_scripts({
+	"@pulsar-damage/shared/weapons.lua",
+	"client/**/*.lua",
+})
+
+server_scripts({
+	"@oxmysql/lib/MySQL.lua",
+	"server/**/*.lua",
+})
+
+shared_scripts({
+	"shared/**/*.lua",
+})
